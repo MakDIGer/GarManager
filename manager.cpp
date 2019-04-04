46036aed0d3436414ccbc72ba52f5ea5aef4dcf7
Nikolay Makarin <makarin@kgdnet.ru>
2019-04-04 10:37:40 +0200

    Started to work over v1.4

--- manager.cpp
+++ manager.cpp
@@ -1,9 +1,9 @@
 /*
  * manager.cpp
  * 
- * Copyright 2015-2016 Nikolay <Nikolay@REX>
+ * Copyright 2015-2018 Nikolay <Nikolay@REX>
  * 
- * Version 1.33
+ * Version 1.4
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -103,7 +103,7 @@ void input_date()
 	cin >> day; cout << endl;
 	if (day == "" or day.length() != 10)
 	{
-		cout << "Error in this date.!!!" << endl;
+		cout << "Error in this date!!!" << endl;
 		input_date();
 	}
 }
@@ -200,7 +200,7 @@ if (check_process(process_name_check))
 void download(string day)
 {
 	cout << endl;
-	buffer_line = "E:\\Garant-FS80\\Download.exe -auto -to " + day + " -dsc 90 -revision -nofinalmessage" + day;
+	buffer_line = "E:\\Garant-FS801\\Download.exe -auto -to " + day + " -dsc 90 -revision -nofinalmessage" + day;
 	cout << buffer_line << endl;
 	run_cmd(buffer_line);
 	cout << "Waiting for the finish of downloading" << endl;
@@ -210,7 +210,7 @@ void download(string day)
 	checking_process("F1Download.run");
 	Sleep(300000);
 	
-	if (check_file("E:\\Garant-FS80\\delta\\","zip"))
+	if (check_file("E:\\Garant-FS801\\delta\\","zip"))
 	{
 		cout << "There is ZIP file." << endl;
 	} else
@@ -304,14 +304,14 @@ int copy_etalon(int copy_mode)
 		return 1;
 	}
 	
-	pcs monster("\\\\monster\\Garant.Srv\\F80\\","\\\\monster\\Garant.Srv\\start.txt");
-	pcs rex("\\\\rex\\Garant.Srv\\F80\\","\\\\rex\\Garant.Srv\\start.txt");
-	pcs rr10("\\\\rr10\\Garant.Srv\\F80\\","\\\\rr10\\Garant.Srv\\start.txt");
-	pcs usepc1("E:\\Garant.SRV\\F80\\","E:\\Garant.SRV\\start.txt");
+	pcs monster("\\\\monster\\Garant.Srv\\F801\\","\\\\monster\\Garant.Srv\\start.txt");
+	pcs rex("\\\\rex\\Garant.Srv\\F801\\","\\\\rex\\Garant.Srv\\start.txt");
+	pcs rr10("\\\\rr10\\Garant.Srv\\F801\\","\\\\rr10\\Garant.Srv\\start.txt");
+	pcs usepc1("E:\\Garant.SRV\\F801\\","E:\\Garant.SRV\\start.txt");
 	
 	data = "";
-	data1 = "E:\\Garant-FS80\\data1\\";
-	data2 = "E:\\Garant-FS80\\data2\\";
+	data1 = "E:\\Garant-FS801\\data1\\";
+	data2 = "E:\\Garant-FS801\\data2\\";
 	
 	if (check_file(data1,"ndt"))
 	{
@@ -345,8 +345,8 @@ int copy_etalon(int copy_mode)
 
 void dataupd()
 {
-	cout << "E:\\Garant-Fs80\\DATAUPD.exe -auto  -nofinalmessage" << endl;
-	run_cmd("E:\\Garant-Fs80\\DATAUPD.exe -auto  -nofinalmessage");
+	cout << "E:\\Garant-Fs801\\DATAUPD.exe -auto  -nofinalmessage" << endl;
+	run_cmd("E:\\Garant-Fs801\\DATAUPD.exe -auto  -nofinalmessage");
 	Sleep(300000);
 	
 	cout << "Checking the updating" << endl;
@@ -359,7 +359,7 @@ int main()
 	string line1, line2;
 	
 	cout << "..............:::Manager of the circle:::............." << endl;
-	cout << ".................... Version 1.33 ...................." << endl;
+	cout << ".................... Version 1.4 ...................." << endl;
 	cout << "......................................................" << endl;
 	cout << time_now();
 	
@@ -370,11 +370,11 @@ int main()
 	run_cmd("rasdial Internet userpc1 20usern2pc1");
 	
 	cout << "Deleting files at the Garant.srv directory............" << endl;
-	clearDIR("E:\\garant.srv\\F80\\");
+	clearDIR("E:\\garant.srv\\F801\\");
 	
 	cout << "Deleting files at the delta directory................." << endl;
-	clearDIR("E:\\Garant-FS80\\delta\\Archive\\");
-	clearDIR("E:\\Garant-FS80\\delta\\");
+	clearDIR("E:\\Garant-FS801\\delta\\Archive\\");
+	clearDIR("E:\\Garant-FS801\\delta\\");
 	
 	cout << "Dowloading the updates for Etalon....................." << endl;
 	cout << time_now();
